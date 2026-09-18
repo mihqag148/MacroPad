9bb0af9516bc8658a149fde47a86f26fecaf8c61
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-18 13:01:38 +0700

    Add custom screensaver animation API

--- src/lumi_ui_config.h
+++ src/lumi_ui_config.h
@@ -3,12 +3,18 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stddef.h>
 
 enum lumi_screensaver_style {
     LUMI_SAVER_TAHOE = 0,
     LUMI_SAVER_MINIMAL = 1,
     LUMI_SAVER_OFF = 2,
 };
+
+#define LUMI_SAVER_FRAME_W 64
+#define LUMI_SAVER_FRAME_H 36
+#define LUMI_SAVER_FRAME_BYTES (LUMI_SAVER_FRAME_W * LUMI_SAVER_FRAME_H)
+#define LUMI_SAVER_MAX_FRAMES 8
 
 void lumi_ui_set_wallpaper(uint8_t r1, uint8_t g1, uint8_t b1,
                            uint8_t r2, uint8_t g2, uint8_t b2);
@@ -19,3 +25,8 @@ void lumi_ui_set_screensaver(bool enabled, uint8_t style,
 void lumi_ui_set_sleep_timeout(uint32_t seconds);
 void lumi_ui_note_activity(void);
 void lumi_ui_set_media_active(bool active);
+
+void lumi_ui_saver_anim_begin(uint8_t frame_count, uint16_t frame_interval_ms);
+void lumi_ui_saver_anim_frame(uint8_t index, const uint8_t *data, size_t len);
+void lumi_ui_saver_anim_end(void);
+void lumi_ui_saver_anim_clear(void);
