e9362c1a44b4e1bc0b5109c9705b4a1fafb14ca9
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-18 13:58:35 +0700

    Add chunked screensaver upload API

--- src/lumi_ui_config.h
+++ src/lumi_ui_config.h
@@ -29,5 +29,7 @@ void lumi_ui_set_media_active(bool active);
 
 void lumi_ui_saver_anim_begin(uint8_t frame_count, uint16_t frame_interval_ms);
 void lumi_ui_saver_anim_frame(uint8_t index, const uint8_t *data, size_t len);
+void lumi_ui_saver_anim_chunk(uint8_t index, uint16_t offset,
+                              const uint8_t *data, size_t len);
 void lumi_ui_saver_anim_end(void);
 void lumi_ui_saver_anim_clear(void);
