11464d51527c7110e8ea864200de3140ebc7ad8d
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-18 13:16:56 +0700

    Add reactive RGB effect and speed control API

--- src/lumi_rgb.h
+++ src/lumi_rgb.h
@@ -9,11 +9,13 @@ enum lumi_rgb_effect_id {
     LUMI_RGB_EFFECT_PURPLE_PINGPONG = 1,
     LUMI_RGB_EFFECT_ORANGE_BLINK = 2,
     LUMI_RGB_EFFECT_SOLID = 3,
-    LUMI_RGB_EFFECT_COUNT = 4,
+    LUMI_RGB_EFFECT_REACTIVE = 4,
+    LUMI_RGB_EFFECT_COUNT = 5,
 };
 
 void lumi_rgb_set_enabled(bool enabled);
 void lumi_rgb_set_brightness_percent(uint8_t percent);
+void lumi_rgb_set_speed_percent(uint8_t percent);
 void lumi_rgb_set_auto(bool enabled);
 void lumi_rgb_set_effect(uint8_t effect);
 void lumi_rgb_set_solid(uint8_t r, uint8_t g, uint8_t b);
