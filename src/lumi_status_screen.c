f8b5af5629612aa9f0ca8b3681edd62848895d42
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-18 17:51:13 +0700

    Restore 4x LVGL canvas scaling for screensaver

--- src/lumi_status_screen.c
+++ src/lumi_status_screen.c
@@ -689,7 +689,7 @@ static void init_screensaver(lv_obj_t *screen) {
         LUMI_SAVER_FRAME_H,
         LV_IMG_CF_TRUE_COLOR);
     lv_obj_align(saver_media_canvas, LV_ALIGN_CENTER, 0, 0);
-    lv_img_set_zoom(saver_media_canvas, 512); /* 160x86 -> 320x172 */
+    lv_img_set_zoom(saver_media_canvas, 1024); /* 80x43 -> 320x172 */
     lv_obj_add_flag(saver_media_canvas, LV_OBJ_FLAG_HIDDEN);
 }
 
