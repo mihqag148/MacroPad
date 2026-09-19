23f0a96ad6645be43fa58448c0230ffca58d6595
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-19 19:58:53 +0700

    Add manual wake API

--- src/lumi_ui_config.h
+++ src/lumi_ui_config.h
@@ -28,6 +28,7 @@ void lumi_ui_set_screensaver(bool enabled, uint8_t style,
                              uint8_t r2, uint8_t g2, uint8_t b2);
 void lumi_ui_set_sleep_timeout(uint32_t seconds);
 void lumi_ui_sleep_now(void);
+void lumi_ui_wake_now(void);
 void lumi_ui_set_screensaver_delay(uint32_t seconds);
 void lumi_ui_show_screensaver_now(void);
 void lumi_ui_note_activity(void);
