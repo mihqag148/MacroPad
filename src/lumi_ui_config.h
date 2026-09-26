51fb5ccbce48557b78def84b56b835995292da26
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-26 08:06:40 +0700

    feat: expose RYNOR sleep state to companion link

--- src/lumi_ui_config.h
+++ src/lumi_ui_config.h
@@ -28,6 +28,7 @@ void lumi_ui_set_screensaver(bool enabled, uint8_t style,
                              uint8_t r2, uint8_t g2, uint8_t b2);
 void lumi_ui_set_sleep_timeout(uint32_t seconds);
 void lumi_ui_set_deep_sleep_timeout(uint32_t seconds);
+bool lumi_ui_is_soft_sleeping(void);
 void lumi_ui_set_rgb_idle_timeout(uint32_t seconds);
 void lumi_ui_sleep_now(void);
 void lumi_ui_wake_now(void);
